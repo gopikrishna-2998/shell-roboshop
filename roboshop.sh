2eab1f04a9fd4ef3c4eb07f772540fae74a7ad08
gopikrishna <gopikrishnamaneem@gmail.com>
2025-10-01 21:03:12 +0530

    robosh

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,6 @@ if [ $instance != "frontend" ]; then
    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-else
-    fi
+fi
     echo "$instance:  $IP"
 done
