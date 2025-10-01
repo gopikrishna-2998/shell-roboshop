8c2ddeb31082e518147c6bcb0994e5849014c5b9
gopikrishna <gopikrishnamaneem@gmail.com>
2025-10-01 21:37:35 +0530

    robosh

---
+++ mongo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+r="\[31m"
+g="\[32m"
+y="\[33m"
+b="\[34m"
+m="\[35m"
+n="\[0m"
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script execution started at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+  echo -e "${r}You should be a root user to execute this script${n}"
+  exit 1
+fi
+
+VALIDATE() {
+  if [ $1 -ne 0 ]; then
+    echo -e "${r}$2 is failed, please check the log file for more information: $LOG_FILE${n}"
+    exit 1
+  else
+    echo -e "${g}$2 is completed successfully${n}"
+  fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo repo file"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "installing mongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enabling mongoDB service"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongoDB service"  
+
+
