b2af2b198ee2dcc19b1ba740019241a36124a218
gopikrishna <gopikrishnamaneem@gmail.com>
2025-10-01 20:43:00 +0530

    robosh

---
+++ roboshop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-03c3303f2e427a5dc"
+for instance in $@
+do 
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type t3.micro --security-group-ids sg-03c3303f2e427a5dc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+if [ $instance != "frontend" ]; then
+   IP=$(aws ec2 describe-instances --instance-ids  i-0597756e7186439ed --query 'Reservations[0].Instances[0].PrivateAddress' --output text)
+else
+    IP=$(aws ec2 describe-instances --instance-ids  i-0597756e7186439ed --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance  $IP"
+done
